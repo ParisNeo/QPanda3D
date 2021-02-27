e82999165499de84c9dd384ebb8dcf4f159d55e8
Maja Bojarska <majabojarska98@gmail.com>
2021-02-27 19:22:01 +0100

    Adapt setup.py to use requirement files.

--- setup.py
+++ setup.py
@@ -1,20 +1,35 @@
+from pathlib import Path
+from typing import Union
+
 import setuptools
+
 with open("README.md", "r") as fh:
     long_description = fh.read()
+
+
+def read_requirements(path: Union[str, Path]):
+    with open(path, "r") as file:
+        return file.read().splitlines()
+
+
+requirements = read_requirements("requirements.txt")
+requirements_dev = read_requirements("requirements_dev.txt")
+
 setuptools.setup(
-     name='QPanda3D',  
-     version='0.2.7',
-     author="Saifeddine ALOUI",
-     author_email="aloui.saifeddine@gmail.com",
-     description="A binding to use Panda3D as a PyQt5 widget",
-     long_description=long_description,
-     long_description_content_type="text/markdown",
-     url="https://github.com/ParisNeo/QPanda3D",
-     packages=setuptools.find_packages(),
-     install_requires=['Panda3D>=1.10.3'],
-     classifiers=[
-         "Programming Language :: Python :: 3",
-         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-         "Operating System :: OS Independent",
-     ],
- )
+    name="QPanda3D",
+    version="0.2.7",
+    author="Saifeddine ALOUI",
+    author_email="aloui.saifeddine@gmail.com",
+    description="A binding to use Panda3D as a PyQt5 widget",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ParisNeo/QPanda3D",
+    packages=setuptools.find_packages(),
+    install_requires=requirements,
+    extras_require={"dev": requirements_dev},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+)
