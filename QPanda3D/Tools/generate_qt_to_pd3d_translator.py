5d269c55ed78d3aea52723b531ad9f6fde94f2f3
Rumeysa <33233212+Rumeysa12@users.noreply.github.com>
2024-05-22 13:12:36 +0300

    Update generate_qt_to_pd3d_translator.py

--- QPanda3D/Tools/generate_qt_to_pd3d_translator.py
+++ QPanda3D/Tools/generate_qt_to_pd3d_translator.py
@@ -1,6 +1,6 @@
-from PyQt5.QtCore import *
-from PyQt5.QtGui import *
-from PyQt5.QtWidgets import *
+from PyQt6.QtCore import *
+from PyQt6.QtGui import *
+from PyQt6.QtWidgets import *
 
 H=Qt.__dict__
 lst = [a for a in H if a.startswith("Key_")]
